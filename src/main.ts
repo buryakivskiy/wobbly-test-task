f05c4a4e5447e694f3034d2c0e2d20af430f2c99
Сергій Буряківський <buryakivskiy@MacBook-Pro-Sergij.local>
2024-05-29 05:59:52 +0300

    fix(port)

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 
 bootstrap();
